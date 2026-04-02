aacb01a6c4edadda7738e7ec319fa890a6309bbd
Yehor Kochetov <yehor.kochetov@plummygames.com>
2026-04-02 16:44:55 +0200

    wire config.toml into session

--- internal/aws/session.go
+++ internal/aws/session.go
@@ -8,9 +8,19 @@ import (
 	"github.com/spf13/viper"
 )
 
+// NewSession loads AWS config with the following priority:
+// flag > env var > ~/.rey/config.toml > default credential chain
 func NewSession(ctx context.Context) (aws.Config, error) {
-	var opts []func(*config.LoadOptions) error
+	opts := []func(*config.LoadOptions) error{
+		config.WithSharedConfigFiles([]string{
+			config.DefaultSharedConfigFilename(),
+		}),
+		config.WithSharedCredentialsFiles([]string{
+			config.DefaultSharedCredentialsFilename(),
+		}),
+	}
 
+	// viper resolves flag > env > config.toml > default automatically
 	if region := viper.GetString("region"); region != "" {
 		opts = append(opts, config.WithRegion(region))
 	}
