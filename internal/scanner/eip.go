7d579551392b88ca1be824cdb6f12e05256c982f
Yehor Kochetov <yehor.kochetov@plummygames.com>
2026-04-05 18:39:11 +0200

    implement EIP Scan method

--- internal/scanner/eip.go
+++ internal/scanner/eip.go
@@ -2,8 +2,10 @@ package scanner
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
 type EIPScanner struct{}
@@ -13,7 +15,36 @@ func (e *EIPScanner) Name() string {
 }
 
 func (e *EIPScanner) Scan(ctx context.Context, cfg aws.Config) ([]DeadResource, error) {
-	return nil, nil
+	client := ec2.NewFromConfig(cfg)
+
+	out, err := client.DescribeAddresses(ctx, &ec2.DescribeAddressesInput{})
+	if err != nil {
+		return nil, fmt.Errorf("describe addresses: %w", err)
+	}
+
+	var results []DeadResource
+	for _, addr := range out.Addresses {
+		if addr.AssociationId != nil {
+			continue
+		}
+
+		tags := make(map[string]string)
+		for _, t := range addr.Tags {
+			tags[aws.ToString(t.Key)] = aws.ToString(t.Value)
+		}
+
+		results = append(results, DeadResource{
+			Type:        "ElasticIP",
+			ID:          aws.ToString(addr.AllocationId),
+			Name:        aws.ToString(addr.PublicIp),
+			Region:      cfg.Region,
+			Reason:      "Not associated to any instance or ENI",
+			MonthlyCost: 3.60,
+			Tags:        tags,
+		})
+	}
+
+	return results, nil
 }
 
 func (e *EIPScanner) EstimateCost(r DeadResource) float64 {
